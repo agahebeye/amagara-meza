b11c11da4f2574002ac79ba02d221bf278c30466
agahebeye <agahebeye@gmail.com>
2023-03-30 06:36:15 +0200

    initial deploy

---
+++ resources/lang/en/navigation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'about' => 'About',
+    'services' => 'Services',
+    'departments' => 'Departments',
+    'doctors' => 'Doctors',
+    'contact' => 'Contact'
+
+];
---
+++ resources/lang/fr/navigation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'home' => 'Accueil',
+    'about' => 'A propos',
+    'services' => 'Services',
+    'departments' => 'Departements',
+    'doctors' => 'Docteurs',
+    'contact' => 'Contact'
+
+];
