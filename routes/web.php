3068e85f649174418a9c8570f09f0c1ff4e9ead4
agahebeye <agahebeye@gmail.com>
2023-03-31 15:05:05 +0200

    create navbar components

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', fn () => view('index'));
+Route::get('/', fn () => view('pages.index'));
 
 
 
 Route::prefix('hospital')->group(function () {
-    Route::get('/', fn () => view('hospitals.index'))->name('hospitals.index');
-    Route::get('/inbox', fn () => view('hospitals.mailbox.inbox'))->name('hospitals.inbox');
-    Route::get('/inbox-detail', fn () => view('hospitals.mailbox.inbox-detail'))->name('hospitals.inbox_detail');
-    Route::get('/compose', fn () => view('hospitals.mailbox.compose'))->name('hospitals.compose');
+    Route::get('/', fn () => view('pages.hospitals.index'))->name('hospitals.index');
+    Route::get('/inbox', fn () => view('pages.hospitals.mailbox.inbox'))->name('hospitals.inbox');
+    Route::get('/inbox-detail', fn () => view('pages.hospitals.mailbox.inbox-detail'))->name('hospitals.inbox_detail');
+    Route::get('/compose', fn () => view('pages.hospitals.mailbox.compose'))->name('hospitals.compose');
 });
