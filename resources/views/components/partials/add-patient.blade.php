474f736239dd2e6099924efaf03aaf4e78eeb757
agahebeye <agahebeye@gmail.com>
2023-04-03 13:44:01 +0200

    wip

---
+++ resources/views/components/partials/add-patient.blade.php
@@ -0,0 +1,163 @@
+   <form id="add-patient-form" class="mfp-hide white-popup-block">
+       <h1>Ajouter nouveau patient</h1>
+       <div class="row mt-5">
+           <div class="col">
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="lastname">Lastname</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="lastname" name="lastname" class="form-control" placeholder="enter your lastname">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="postname">Postname</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="postname" name="postname" class="form-control" placeholder="enter your postname">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="firstname">Firstname</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="firstname" name="firstname" class="form-control" placeholder="enter your firstname">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="birthplace">Place of Birth</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="birthplace" name="birthplace" class="form-control" placeholder="enter your place of birth">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="bdate">Date of Birth</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="date" id="bdate" name="bdate" class="form-control mydatepicker" placeholder="enter your birth date">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-sm-12">Gender</label>
+                   <div class="col-sm-12">
+                       <select class="form-control">
+                           <option>Select Gender</option>
+                           <option selected="selected">Male</option>
+                           <option>Female</option>
+                       </select>
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-sm-12">Marital Status</label>
+                   <div class="col-sm-12">
+                       <select class="form-control">
+                           <option disabled>Select marital status</option>
+                           <option selected="selected">Bachelor</option>
+                           <option>Married</option>
+                           <option>Divorced</option>
+                           <option>Widowed</option>
+                       </select>
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="father_name">Father's name</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="father_name" name="father_name" class="form-control" placeholder="enter your father's name">
+                   </div>
+               </div>
+           </div>
+           <div class="col">
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="mother_name">Mother's name</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="mother_name" name="mother_name" class="form-control" placeholder="enter your mother's name">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="relative">Relative</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="relative" name="relative" class="form-control" placeholder="enter your firstname">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="nationality">Nationality</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="nationality" name="nationality" class="form-control" placeholder="enter your nationality">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="country">Country</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="country" name="country" class="form-control" placeholder="enter your country">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="province">Province</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="province" name="province" class="form-control" placeholder="enter your province">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="town">Town</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="town" name="town" class="form-control" placeholder="enter your town">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="municipality">Municipality</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="municipality" name="municipality" class="form-control" placeholder="enter your municipality">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="neighborhood">Neighborhood</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="neighborhood" name="neighborhood" class="form-control" placeholder="enter your neighborhood">
+                   </div>
+               </div>
+
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="avenue">Avenue</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="avenue" name="avenue" class="form-control" placeholder="enter your avenue">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="house_number">House number</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="house_number" name="house_number" class="form-control" placeholder="enter your house_number">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="profession">Profession</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="profession" name="profession" class="form-control" placeholder="enter your profession">
+                   </div>
+               </div>
+               <div class="form-group">
+                   <label class="col-md-12 mt-4" for="phone">Phone</span>
+                   </label>
+                   <div class="col-md-12">
+                       <input type="text" id="phone" name="phone" class="form-control" placeholder="enter your phone">
+                   </div>
+               </div>
+           </div>
+       </div>
+
+       <div class="row justify-content-center mt-5">
+           <button type="submit" class="btn btn-info px-5" id='submit-patient'>Submit</button>
+       </div>
+
+   </form>
